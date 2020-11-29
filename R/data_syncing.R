c06038c837912ef705e9dff082db2afd97923f28
thartbm <thartbm@gmail.com>
2020-11-29 17:43:54 -0500

    added a file check function that *should* never have anything to report (it checks if the files downloaded from OSF have all the files listed in the master sheet of files for those groups & sections)

--- R/data_syncing.R
+++ R/data_syncing.R
@@ -40,5 +40,79 @@ syncData <- function(groups='all', sections='all', overwrite=TRUE) {
     unzip(zipfile = folderfilename, exdir = 'data/raw/')
     
   }
-    
+  
+  checkFiles(groups=groups, sections=sections)
+  
 }
+
+checkFiles <- function(groups='all', sections='sections', verbose=F) {
+  
+  pfiles <- read.csv('data/files.csv', stringsAsFactors = F)
+  fileURLs <- read.csv('data/urls.csv', stringsAsFactors = F)
+  
+  if (groups[1] != 'all') {
+    fileURLs <- fileURLs[which(fileURLs$group %in% groups),]
+  }
+  
+  if (sections[1] != 'all') {
+    fileURLs <- fileURLs[which(fileURLs$section %in% sections),]
+  }
+  
+  
+  sectionfiles <- list('aligned'=c('aligned_training',
+                                   'aligned_nocursor',
+                                   'aligned_passivelocalization',
+                                   'aligned_activelocalization'),
+                       'rotated'=c('rotated_training',
+                                   'rotated_nocursor',
+                                   'rotated_passivelocalization',
+                                   'rotated_activelocalization'),
+                       'localization-reaches'=c('rotated_passivereach',
+                                                'rotated_activereach',
+                                                'aligned_passivereach',
+                                                'aligned_activereach')
+  )
+  
+  Nmissing <- 0
+  
+  for (rown in c(1:dim(fileURLs)[1])) {
+    
+    group <- fileURLs$group[rown]
+    section <- fileURLs$section[rown]
+  
+    filetypes <- sectionfiles[[section]]
+    
+    participants <- pfiles$participant[which(pfiles$group == group)]
+    
+    for (filetype in filetypes) {
+      
+      for (participant in participants) {
+        
+        prow <- which(pfiles$participant == participant)
+        
+        pfile <- pfiles[prow,filetype]
+        
+        if (nchar(pfile) > 0) {
+          
+          # this file *should* be there:
+          filename <- sprintf('data/raw/%s/%s/%s',group,participant,pfile)
+          
+          if (!file.exists(filename)) {
+            if (verbose) { cat(sprintf('WARNING: missing file? %s\n',filename)) }
+            Nmissing <- Nmissing + 1
+          }
+          
+        }
+        
+      }
+      
+    }
+    
+  }
+  
+  if (Nmissing) {
+    cat(sprintf('WARNING: %d missing files!\n', Nmissing))
+  }
+  
+}
+
