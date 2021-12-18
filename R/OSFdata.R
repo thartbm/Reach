9c486bf6c4e0c9ec2ae3d8fa9bab5eb6246ac233
thartbm <thartbm@gmail.com>
2021-12-18 12:58:03 -0500

    implement option to remove zip files after unzipping

--- R/OSFdata.R
+++ R/OSFdata.R
@@ -108,7 +108,14 @@ unzipZips <- function(filelist,folder,removezips=FALSE) {
       
       # check if it is a zip file:
       ext <- tools::file_ext(filename)
-      print(ext)
+      if (ext == 'zip') {
+        utils::unzip(zipfile=sprintf('%s/%s',folder,filename),
+                     exdir=folder)
+      }
+      
+      if (removezips) {
+        file.remove(sprintf('%s/%s',folder,filename))
+      }
       
     }
     
