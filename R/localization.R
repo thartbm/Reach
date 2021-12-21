c872c9e6c153129fd696ef29e3d75e8bb840df59
thartbm <thartbm@gmail.com>
2021-12-21 14:15:15 -0500

    split preparation of data for splines, spline fitting and prediction into 3 subfunctions... SD calculation calls on these three, but so may an estimate of localization biases throughout the workspace

--- R/localization.R
+++ R/localization.R
@@ -52,21 +52,70 @@ circleErrors <- function(par,X,Y,r) {
   
 }
 
-localizationSD <- function(df, unit='cm', locvar='tap', handvar='hand', r=1, CC=TRUE, spar=0.50) {
+#' Get the standard deviation of localization using a spline as 'mean'.
+#' 
+#' @param df Data frame with localization data.
+#' @param unit The unit of position data (default: \code{cm}).
+#' @param locvar The variable name of localization responses (default: \code{tap}).
+#' @param handvar The variable name of hand position (default: \code{hand}).
+#' @param r Radius of localization arc (default: 12).
+#' @param CC (boolean) Perform circle correction (default: \code{TRUE}).
+#' @param spar Smoothing parameter for the spline (0,1) (default: \code{0.50}). 
+#' @return A spline object that predicts localization errors over reach angle.
+#' @export
+getLocalizationSD <- function(df, unit='cm', locvar='tap', handvar='hand', r=1, CC=TRUE, spar=0.50) {
+
+  df <- prepareSplineVariables(df, CC=CC, handvar=handvar, locvar=locvar, unit=unit, r=r)
   
+  spl <- getLocalizationSpline(df, spar=spar)
+  
+  # predict
+  # get differences with prediction
+  # convert to "standard deviation"
+  
+}
+
+#' Get a smoothed spline object fit on localization errors over reach angles.
+#' 
+#' @param df Data frame with localization data, this contains at least the
+#' variables: \code{reachangle_deg} and \code{localizationerror_deg}.
+#' @param spar Smoothing parameter for the spline (0,1) (default: \code{0.50}). 
+#' @return A spline object that predicts localization errors over reach angle.
+#' @export
+getLocalizationSpline <- function(df, spar=0.50) {
+  
+  # fit a smooth spline to predict localization error by reach angle:
+  spl <- smooth.spline(x=df$reachangle_deg, y=df$localizationerror_deg, spar=spar, keep.data=F )
+  
+  return(spl)
+  
+}
+
+prepareSplineVariables <- function(df, CC=TRUE, handvar='hand', locvar='tap', unit='cm',r=1) {
+  
+  # removed non-good data:
   if ('selected' %in% names(df)) {
     df <- df[which(df$selected == 1),]
   }
   
+  # perform circle correction if specified:
   if (CC) {
     df <- circleCorrect(df, unit=unit, var=locvar,r=r)
   }
   
-  yvar <- sprintf('%sy_%s',locvar,unit)
-  xvar <- sprintf('%sx_%s',locvar,unit)
+  # get reach endpoint angles:
+  handyvar <- sprintf('%sy_%s',handvar,unit)
+  handxvar <- sprintf('%sx_%s',handvar,unit)
+  df$reachangle_deg <- (atan2(df[,handyvar],df[,handxvar])/pi)*180
   
-  df$localizationangle_deg <- (atan2(df[,yvar],df[,xvar])/pi)*180
+  # get localization angle:
+  locyvar <- sprintf('%sy_%s',locvar,unit)
+  locxvar <- sprintf('%sx_%s',locvar,unit)
+  df$localizationangle_deg <- (atan2(df[,locyvar],df[,locxvar])/pi)*180
   
-  spl <- smooth.spline(x=df$targetangle_deg, y=df$localizationangle_deg, spar=spar, keep.data=F )
+  # get localization error:
+  df$localizationerror_deg <- df$localizationangle_deg - df$reachangle_deg
+  
+  return(df)
   
 }
