7f564c5a9dbbaeb281201ce56622c1736db5c358
thartbm <thartbm@gmail.com>
2021-12-22 14:29:12 -0500

    simplified and compartmentalized spline variable preparation

--- R/localization.R
+++ R/localization.R
@@ -95,6 +95,19 @@ getLocalizationSpline <- function(df, spar=0.50) {
   
 }
 
+#' Prepare data for smoothed spline fitting.
+#' 
+#' @param df Data frame with localization data, this contains at least the
+#' variables: \code{reachangle_deg} and \code{localizationerror_deg}.
+#' @param CC (boolean) Apply circleCorrect or not (default: \code{TRUE}).
+#' @param handvar Part of column names specifying reach endpoints (default: \code{hand}).
+#' @param locvar Part of column names specifying hand location (default: \code{tap}).
+#' @param unit Unit of the four location variables (default: \code{cm}).
+#' @param r Radius for the circleCorrect procedure (default: \code{1})
+#' @return Hand lcalization data frame extended with columns to be used for
+#' smooth spline fitting.
+#' @details 
+#' @export
 prepareSplineVariables <- function(df, CC=TRUE, handvar='hand', locvar='tap', unit='cm',r=1) {
   
   # removed non-good data:
@@ -104,21 +117,84 @@ prepareSplineVariables <- function(df, CC=TRUE, handvar='hand', locvar='tap', un
   
   # perform circle correction if specified:
   if (CC) {
-    df <- circleCorrect(df, unit=unit, var=locvar,r=r)
+    df <- circleCorrect( df, 
+                         unit=unit, 
+                         var=locvar, 
+                         r=r)
   }
   
-  # get reach endpoint angles:
-  handyvar <- sprintf('%sy_%s',handvar,unit)
-  handxvar <- sprintf('%sx_%s',handvar,unit)
-  df$reachangle_deg <- (atan2(df[,handyvar],df[,handxvar])/pi)*180
+  # assemble hand/reach column names:
+  handy <- df[,sprintf('%sy_%s',handvar,unit)]
+  handx <- df[,sprintf('%sx_%s',handvar,unit)]
   
-  # get localization angle:
-  locyvar <- sprintf('%sy_%s',locvar,unit)
-  locxvar <- sprintf('%sx_%s',locvar,unit)
-  df$localizationangle_deg <- (atan2(df[,locyvar],df[,locxvar])/pi)*180
+  # get reach endpoint angles:
+  reachangle_deg <- (atan2(handy,handx)/pi)*180
+  df$reachangle_deg <- reachangle_deg
+  
+  # get the localization coordinates in a separate data frame:
+  tempdf <- data.frame('X'      = df[,sprintf('%sx_%s',locvar,unit)],
+                       'Y'      = df[,sprintf('%sy_%s',locvar,unit)],
+                       'relang' = reachangle_deg)
+  
+  # rotate localization coordinates relative to reach angle
+  # (such that reach angle is 0 for all localizations)
+  tempdf <- reorientCoordinatesBy(tempdf,X='X',Y='Y',orient='relang',CCW=FALSE)
+  
+  # get localization error as angle relative to reach angle:
+  localizationerror_deg <- (atan2(tempdf$Y,tempdf$X)/pi)*180
+  
+  # do we need this at all?
+  df$localizationangle_deg <- df$reachangle_deg + localizationangle_deg
   
   # get localization error:
-  df$localizationerror_deg <- df$localizationangle_deg - df$reachangle_deg
+  df$localizationerror_deg <- localizationerror_deg
+  
+  return(df)
+  
+}
+
+
+#' Rotate coordinates in a data frame relative to reference angle.
+#' 
+#' @param df Data frame with at least 2 columns.
+#' @param X name of column with X coordinates.
+#' @param Y name of column with Y coordinates.
+#' @param orient Either numeric specifying one angle to use for all coordinates
+#' or a string specifying the column with reference angles.
+#' @param CCW (boolean)
+#' @return Data frame with X and Y rotated by the angles in \code{orient}.
+#' @export
+reorientCoordinatesBy <- function(df,X,Y,orient,CCW=FALSE) {
+  
+  if (!CCW) {
+    multiply <- -1
+  } else {
+    multiply <-  1
+  }
+  
+  if (is.character(orient)) {
+    if (!orient %in% names(df)) {
+      cat(sprintf('"%s" needs to be a column in the data frame\n',orient))
+    } else if (! is.numeric(df[,orient])) {
+      cat(sprintf('column "%s" needs to be numeric\n',orient))
+    } else {
+      
+      orientvals <- unique(df[,orient])
+      for (orientval in orientvals) {
+        or.idx <- which(df[,orient] == orientval)
+        coords <- rotateCoordinates(data.frame('X'=df[or.idx,X],'Y'=df[or.idx,Y]),
+                                    angle=orient*multiple)
+        df[or.idx,X] <- coords$X
+        df[or.idx,Y] <- coords$Y
+      }
+      
+    }
+  } else if (is.numeric(orient)) {
+    coords <- rotateCoordinates(data.frame('X'=df[,X],'Y'=df[,Y]),
+                                angle=orient*multiple)
+    df[,X] <- coords$X
+    df[,Y] <- coords$Y
+  }
   
   return(df)
   
