3914bd30edbe9c5c3481280bcc805f0bd18eafce
thartbm <thartbm@gmail.com>
2022-01-03 09:45:48 -0500

    add documentation for rm.Extr option in localizationSD function

--- R/localization.R
+++ R/localization.R
@@ -60,7 +60,9 @@ circleErrors <- function(par,X,Y,r) {
 #' @param handvar The variable name of hand position (default: \code{hand}).
 #' @param r Radius of localization arc (default: 12).
 #' @param CC (boolean) Perform circle correction (default: \code{TRUE}).
-#' @param spar Smoothing parameter for the spline (0,1) (default: \code{0.50}). 
+#' @param spar Smoothing parameter for the spline (0,1) (default: \code{0.85}).
+#' @param rm.Extr (boolean) Remove the highest and lowest reach angles before 
+#' spline interpolation (after fitting the spline). 
 #' @return A spline object that predicts localization errors over reach angle.
 #' @export
 localizationSD <- function(df, unit='cm', locvar='tap', handvar='hand', r=1, CC=TRUE, spar=0.85, rm.Extr=FALSE) {
