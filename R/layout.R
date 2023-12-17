5c1b05c2fc9216e215e5edb1f5965cc0ff2b4719
Marius 't Hart <thartbm@gmail.com>
2023-12-17 10:41:28 -0500

    added layout functions: calculate poster guide positions, and color manipulation functions

--- R/layout.R
+++ R/layout.R
@@ -34,3 +34,100 @@ posterGuides <- function(size          = 279.4,
   return(guides)
   
 }
+
+# colors -----
+
+#' @title Mix two colors
+#' @param a First color
+#' @param b Second color
+#' @param balance Numeric vector of length 2, Weights of the two colors
+#' @return A weighted average of the two colors in RGB space
+#' @description Mixes two colors in RGB space using weights
+#' @export
+colorMix <- function(a='#41ffc9', b='#1d7791', balance=c(1,1)) {
+  
+  a <- col2rgb(a,alpha=TRUE)/255
+  b <- col2rgb(b,alpha=TRUE)/255
+  
+  w <- balance / sum(balance)
+  
+  R <- (a[1]*w[1]) + (b[1]*w[2])
+  G <- (a[2]*w[1]) + (b[2]*w[2])
+  B <- (a[3]*w[1]) + (b[3]*w[2])
+  A <- (a[4]*w[1]) + (b[4]*w[2])
+  
+  return(rgb(red=R, green=G, blue=B, alpha=A))
+  
+}
+
+#' @title Change the saturation of a color
+#' @param col Color
+#' @param sat.mult Multiply saturation with this number
+#' @return The input color with the saturation multiplied.
+#' @description Converts the input color to HSV space, then multiplies the saturation
+#' by the saturation multiplier.
+#' @export
+colorSaturation <- function(col='#41ffc9', sat.mult=1.25) {
+  
+  inter <- rgb2hsv(col2rgb(col))
+  
+  sat.mult  <- max(0, sat.mult) 
+  inter[2,] <- min(1, inter[2,] * sat.mult)
+  
+  return(hsv(inter[1,], inter[2,], inter[3,]))
+  
+}
+
+
+## Transparent colors
+## Mark Gardener 2015
+## www.dataanalytics.org.uk
+# t_col <- function(color, percent = 50, name = NULL) {
+#   #      color = color name
+#   #    percent = % transparency
+#   #       name = an optional name for the color
+#   
+#   ## Get RGB values for named color
+#   rgb.val <- col2rgb(color)
+#   
+#   ## Make new color using input color as base and alpha set by transparency
+#   t.col <- rgb(rgb.val[1], rgb.val[2], rgb.val[3],
+#                max = 255,
+#                alpha = (100 - percent) * 255 / 100,
+#                names = name)
+#   
+#   ## Save the color
+#   #invisible(t.col)
+#   return(t.col)
+# }
+
+
+#' @title Set or change the alpha (transparency) of a color
+#' @param col Color
+#' @param alpha Alpha value to apply
+#' @return An RGBA color
+#' @description Sets the alpha of an RGB(A) color
+#' @export
+colorAlpha <- function(col, alpha = 34) {
+  
+  # store the names:
+  colornames <- names(col)
+  
+  # print(colornames)
+  
+  # get RGB values for named color
+  rgb.val <- t(col2rgb(col))
+  
+  # add alpha column:
+  # rgb.val <- rbind(rgb.val, rep(alpha, dim(rgb.val)[1]))
+  
+  ## Make new color using input color as base and alpha set by transparency
+  t.col <- rgb(rgb.val,
+               alpha = alpha,
+               max   = 255)
+  
+  names(t.col) <- colornames
+  
+  return(t.col)
+  
+}
