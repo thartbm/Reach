fc200c7581728af2731ea23b46c5b2456654fe4e
Marius 't Hart <thartbm@gmail.com>
2023-12-18 20:20:56 -0500

    update to fitting exponentials with N0 set

--- R/models.R
+++ R/models.R
@@ -469,6 +469,10 @@ exponentialFit <- function(signal, timepoints=length(signal), mode='learning', g
                               'N0'     = parvals * diff(asymptoteRange) + asymptoteRange[1] )
     lo <- c(0,asymptoteRange[1])
     hi <- c(1,asymptoteRange[2])
+  } else {
+    searchgrid <- expand.grid('lambda' = parvals )
+    lo <- c(0)
+    hi <- c(1)
   }
   # evaluate starting positions:
   MSE <- apply(searchgrid, FUN=exponentialMSE, MARGIN=c(1), signal=signal, timepoints=timepoints, mode=mode, setN0=setN0)
