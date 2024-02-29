9ada3801865fee0a1f613f87b4e87da548953ea1
Marius 't Hart <thartbm@gmail.com>
2024-02-29 09:27:03 -0500

    removed function

--- R/OSFdata.R
+++ R/OSFdata.R
@@ -128,82 +128,82 @@ unzipZips <- function(filelist,folder,removezips=FALSE) {
 }
 
 
-#' Check if csv files from requested groups and sections exist
-#' 
-#' @param groups vector of group names for which to download data (default: 'all')
-#' @param sections vector of task sections for which to download data (default: 'all')
-#' @param verbose boolean, if \code{TRUE}: print names of all missing files
-#' @return empty
-#' @examples
-#' #dataFilecheck(groups='all', sections=c('aligned'), verbose=TRUE)
-dataFilecheck <- function(groups='all', sections='sections', verbose=FALSE) {
-  
-  utils::data('files', package='handlocs')
-  utils::data('urls', package='handlocs')
-  
-  if (groups[1] != 'all') {
-    urls <- urls[which(urls$group %in% groups),]
-  }
-  
-  if (sections[1] != 'all') {
-    urls <- urls[which(urls$section %in% sections),]
-  }
-  
-  
-  sectionfiles <- list('aligned'=c('aligned_training',
-                                   'aligned_nocursor',
-                                   'aligned_passivelocalization',
-                                   'aligned_activelocalization'),
-                       'rotated'=c('rotated_training',
-                                   'rotated_nocursor',
-                                   'rotated_passivelocalization',
-                                   'rotated_activelocalization'),
-                       'localization-reaches'=c('rotated_passivereach',
-                                                'rotated_activereach',
-                                                'aligned_passivereach',
-                                                'aligned_activereach')
-  )
-  
-  Nmissing <- 0
-  
-  for (rown in c(1:dim(urls)[1])) {
-    
-    group <- urls$group[rown]
-    section <- urls$section[rown]
-  
-    filetypes <- sectionfiles[[section]]
-    
-    participants <- files$participant[which(files$group == group)]
-    
-    for (filetype in filetypes) {
-      
-      for (participant in participants) {
-        
-        prow <- which(files$participant == participant)
-        
-        pfile <- files[prow,filetype]
-        
-        if (nchar(pfile) > 0) {
-          
-          # this file *should* be there:
-          filename <- sprintf('data/%s/%s/%s',group,participant,pfile)
-          
-          if (!file.exists(filename)) {
-            if (verbose) { cat(sprintf('WARNING: missing file? %s\n',filename)) }
-            Nmissing <- Nmissing + 1
-          }
-          
-        } # check existence of files that should be there
-        
-      } # end loop through participants
-      
-    } # end loop through filetypes
-    
-  } # end loop through section files
-  
-  if (Nmissing) {
-    cat(sprintf('WARNING: %d missing files! (set verbose=T to see which)\n', Nmissing))
-  }
-  
-}
+#' #' Check if csv files from requested groups and sections exist
+#' #' 
+#' #' @param groups vector of group names for which to download data (default: 'all')
+#' #' @param sections vector of task sections for which to download data (default: 'all')
+#' #' @param verbose boolean, if \code{TRUE}: print names of all missing files
+#' #' @return empty
+#' #' @examples
+#' #' #dataFilecheck(groups='all', sections=c('aligned'), verbose=TRUE)
+#' dataFilecheck <- function(groups='all', sections='sections', verbose=FALSE) {
+#'   
+#'   utils::data('files', package='handlocs')
+#'   utils::data('urls', package='handlocs')
+#'   
+#'   if (groups[1] != 'all') {
+#'     urls <- urls[which(urls$group %in% groups),]
+#'   }
+#'   
+#'   if (sections[1] != 'all') {
+#'     urls <- urls[which(urls$section %in% sections),]
+#'   }
+#'   
+#'   
+#'   sectionfiles <- list('aligned'=c('aligned_training',
+#'                                    'aligned_nocursor',
+#'                                    'aligned_passivelocalization',
+#'                                    'aligned_activelocalization'),
+#'                        'rotated'=c('rotated_training',
+#'                                    'rotated_nocursor',
+#'                                    'rotated_passivelocalization',
+#'                                    'rotated_activelocalization'),
+#'                        'localization-reaches'=c('rotated_passivereach',
+#'                                                 'rotated_activereach',
+#'                                                 'aligned_passivereach',
+#'                                                 'aligned_activereach')
+#'   )
+#'   
+#'   Nmissing <- 0
+#'   
+#'   for (rown in c(1:dim(urls)[1])) {
+#'     
+#'     group <- urls$group[rown]
+#'     section <- urls$section[rown]
+#'   
+#'     filetypes <- sectionfiles[[section]]
+#'     
+#'     participants <- files$participant[which(files$group == group)]
+#'     
+#'     for (filetype in filetypes) {
+#'       
+#'       for (participant in participants) {
+#'         
+#'         prow <- which(files$participant == participant)
+#'         
+#'         pfile <- files[prow,filetype]
+#'         
+#'         if (nchar(pfile) > 0) {
+#'           
+#'           # this file *should* be there:
+#'           filename <- sprintf('data/%s/%s/%s',group,participant,pfile)
+#'           
+#'           if (!file.exists(filename)) {
+#'             if (verbose) { cat(sprintf('WARNING: missing file? %s\n',filename)) }
+#'             Nmissing <- Nmissing + 1
+#'           }
+#'           
+#'         } # check existence of files that should be there
+#'         
+#'       } # end loop through participants
+#'       
+#'     } # end loop through filetypes
+#'     
+#'   } # end loop through section files
+#'   
+#'   if (Nmissing) {
+#'     cat(sprintf('WARNING: %d missing files! (set verbose=T to see which)\n', Nmissing))
+#'   }
+#'   
+#' }
 
